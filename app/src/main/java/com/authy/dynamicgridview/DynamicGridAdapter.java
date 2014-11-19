b4731a6eaffcabe9a167c6822ad2087ebd43a9b2
fhur <fernando@authy.com>
2014-11-18 19:12:01 -0500

    Added methods to DynamicGridAdapter

--- app/src/main/java/com/authy/dynamicgridview/DynamicGridAdapter.java
+++ app/src/main/java/com/authy/dynamicgridview/DynamicGridAdapter.java
@@ -7,6 +7,8 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,10 +18,14 @@ import java.util.List;
 public abstract class DynamicGridAdapter<T> extends BaseAdapter {
 
     private List<T> data;
+    private int draggingPosition;
+    private int draggingOver;
 
     public DynamicGridAdapter(){
         super();
         data = new ArrayList<T>();
+        draggingPosition = -1;
+        draggingOver = -1;
     }
 
     @Override
@@ -36,14 +42,23 @@ public abstract class DynamicGridAdapter<T> extends BaseAdapter {
         data.add(item);
     }
 
-    public void addAll(List<T> items){
+    public void addAll(Collection<T> items){
+        data.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void setAll(Collection<T> items){
+        data.clear();
         data.addAll(items);
         notifyDataSetChanged();
     }
 
     public void addAll(T... items){
-        Collections.addAll(data, items);
-        notifyDataSetChanged();
+        addAll(Arrays.asList(items));
+    }
+
+    public void setAll(T... items){
+        setAll(Arrays.asList(items));
     }
 
     public void swap(int pos1, int pos2){
@@ -51,11 +66,28 @@ public abstract class DynamicGridAdapter<T> extends BaseAdapter {
         T second = data.get(pos2);
         data.set(pos1, second);
         data.set(pos2, first);
-        notifyDataSetChanged();
     }
 
     @Override
     public long getItemId(int position) {
         return -1;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = getViewItem(position, convertView, parent);
+        view.setVisibility(draggingPosition == position ? View.INVISIBLE : View.VISIBLE);
+        view.setAlpha(draggingOver == position ? 0.5f : 1.0f);
+        return view;
+    }
+
+    public abstract View getViewItem(int position, View convertView, ViewGroup parent);
+
+    public void setDraggingPosition(int draggingPosition) {
+        this.draggingPosition = draggingPosition;
+    }
+
+    public void setDraggingOver(int draggingOver) {
+        this.draggingOver = draggingOver;
     }
 }
